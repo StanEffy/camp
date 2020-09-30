fc8b2fa6ec01872361dd8a89d5c24de42d78455b
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2020-09-30 15:43:02 +0300

    compressed images added two more

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ let mobileMenuOpenClose = function(evt){
 }
 
 toggleMenuButton.addEventListener('click', mobileMenuOpenClose);
+
+const getDate = new Date();
+//const month = getDate.getMonth();
+const month = 11;
+const body = document.querySelector('body');
+
+if(month === 0 || month === 1 || month === 11) {
+	//winter
+	document.body.style.backgroundImage = 'url(img/winter-birch.jpg)';
+} else if(month === 2 || month === 3 || month === 4){
+	//spring
+	document.body.style.backgroundImage = 'url(img/spring-birch.jpg)';
+} else if(month === 5 || month === 6 || month === 7) {
+	//summer
+	document.body.style.backgroundImage = 'url(img/leaves-4325098.jpg)';
+} else {
+	//autumn
+	document.body.style.backgroundImage = 'url(img/leaves-1036560.jpg)';
+}
