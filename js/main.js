826f058ec8ae0f2ae8f403f29e6e86910b3d2fdf
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2020-10-01 17:12:51 +0300

    documents added, menu in mobile mended and header extended

--- js/main.js
+++ js/main.js
@@ -3,18 +3,6 @@
 let toggleMenuButton = document.querySelector('.menu__toggle-button');
 let mainMenu = document.querySelector('.main-header__list');
 
-let mobileMenuOpenClose = function(evt){
-	if(mainMenu.classList.contains('main-header__list--active')){
-		mainMenu.classList.remove('main-header__list--active');
-		toggleMenuButton.classList.remove('menu__toggle-button--active');
-	} else {
-		mainMenu.classList.add('main-header__list--active');
-		toggleMenuButton.classList.add('menu__toggle-button--active');
-	}
-}
-
-toggleMenuButton.addEventListener('click', mobileMenuOpenClose);
-
 const getDate = new Date();
 const month = getDate.getMonth();
 
@@ -33,3 +21,28 @@ if(month === 0 || month === 1 || month === 11) {
 	//autumn
 	document.body.style.backgroundImage = 'url(img/leaves-1036560.jpg)';
 }
+var i = 0;
+// const mobileMenu = document.querySelectorAll('.main-header__list-item');
+// const subMenu = document.querySelectorAll('.inner-list');
+// mobileMenu.forEach(menu => menu.addEventListener('click', function (evt) {
+// 	evt.preventDefault();
+// 	for(let i = 0; i < mobileMenu.length; i++) {
+// 		if(mobileMenu[i].classList.contains('main-header__list-item--active')) {
+// 			mobileMenu[i].classList.remove('main-header__list-item--active');
+// 		}
+// 	}
+// 	menu.classList.toggle('main-header__list-item--active');
+// }))
+
+let mobileMenuOpenClose = function(evt){
+	if(mainMenu.classList.contains('main-header__list--active')){
+		mainMenu.classList.remove('main-header__list--active');
+		toggleMenuButton.classList.remove('menu__toggle-button--active');
+		// mobileMenu.forEach(element => element.classList.remove('main-header__list-item--active'))
+	} else {
+		mainMenu.classList.add('main-header__list--active');
+		toggleMenuButton.classList.add('menu__toggle-button--active');
+	}
+}
+
+toggleMenuButton.addEventListener('click', mobileMenuOpenClose);
