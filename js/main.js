671afcc0cdfa6feb3ed709a770b7dbb23488789e
Stan_Effy <47476119+StanEffy@users.noreply.github.com>
2020-10-06 19:23:12 +0300

    fixed slider for mobile

--- js/main.js
+++ js/main.js
@@ -47,32 +47,82 @@ let mobileMenuOpenClose = function(evt){
 
 toggleMenuButton.addEventListener('click', mobileMenuOpenClose);
 
-const galleryShowNext = document.querySelector('.pictures-container__button--right');
-const galleryShowPrevious = document.querySelector('.pictures-container__button--left');
-const pictureContainer = document.querySelector('.pictures-container');
-const picsArray = pictureContainer.querySelectorAll('.news-picture');
+const galleryShowNext = document.querySelectorAll('.pictures-container__button--right');
+const galleryShowPrevious = document.querySelectorAll('.pictures-container__button--left');
+const pictureContainer = document.querySelectorAll('.pictures-container');
+
 let index = 0;
-galleryShowNext.addEventListener('click', function () {
-		if(index === picsArray.length - 1){
-			picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
-			index = 0;
-			picsArray[index].classList.add('news-picture--active')
-		} else {
-			picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
-			picsArray[index+1].classList.add('news-picture--active');
-			index += 1;
-		}
-	console.log(index);
-});
-galleryShowPrevious.addEventListener('click', function () {
-	if(index === 0 ){
-		picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
-		index = picsArray.length - 1;
-		picsArray[index - 1].classList.add('news-picture--active');
-	} else {
-		picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
-		picsArray[index - 1].classList.add('news-picture--active');
-		index -= 1;
-	}
-	console.log(index);
-});
+
+let showNextPic = function(rightButton) {
+	rightButton.forEach(element => {
+		element.addEventListener('click', function () {
+			let parent = element.parentNode;
+			let picsArray = parent.querySelectorAll('.news-picture');
+			if(index === picsArray.length - 1){
+				picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
+				index = 0;
+				picsArray[index].classList.add('news-picture--active')
+			} else {
+				picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
+				picsArray[index+1].classList.add('news-picture--active');
+				index += 1;
+			}
+			console.log(index);
+		});
+	});
+}
+
+let showPrevPic = function(leftButton) {
+	leftButton.forEach( element => {
+		let parent = element.parentNode;
+		let picsArray = parent.querySelectorAll('.news-picture');
+		element.addEventListener('click', function () {
+			if(index === 0 ){
+				picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
+				index = picsArray.length - 1;
+				picsArray[index - 1].classList.add('news-picture--active');
+			} else {
+				picsArray.forEach(elem => elem.classList.remove('news-picture--active'));
+				picsArray[index - 1].classList.add('news-picture--active');
+				index -= 1;
+			}
+
+		});
+	})
+}
+showNextPic(galleryShowNext);
+showPrevPic(galleryShowPrevious);
+
+
+const picturesFullscreen = document.querySelector('.pictures-container--fullscreen');
+
+const fullscreen = document.querySelectorAll('.pictures-container__button--fullscreen');
+
+
+
+
+fullscreen.forEach(element => element.addEventListener('click', function () {
+	let newsArticle = this.parentNode.parentNode;
+	console.log(newsArticle);
+	(newsArticle.appendChild(this.parentNode.cloneNode(true))).classList.add('pictures-container--fullscreen');
+	let fullscreenMode = newsArticle.querySelector('.pictures-container--fullscreen');
+	let button = document.createElement('button');
+	button.classList.add('pictures-container__button--close');
+	button.classList.add('pictures-container__button');
+	fullscreenMode.appendChild(button);
+
+	//what a shame to do it this way, but the struggle is real
+	//one day I will improve this code
+	let right = fullscreenMode.querySelectorAll('.pictures-container__button--right');
+	let left = fullscreenMode.querySelectorAll('.pictures-container__button--left');
+
+	let buttonClose = fullscreenMode.querySelector('.pictures-container__button--close')
+	buttonClose.addEventListener('click', function () {
+		fullscreenMode.remove();
+	})
+	showNextPic(right);
+	showPrevPic(left);
+}))
+
+
+document.addEventListener('click', evt => console.log(evt.target))
